e8637915831d96d281e7a01e8c2ae8bb3ce03d16
bcl <1010714847@qq.com>
2021-01-20 15:13:13 +0800

    fix-message:redis项目集成jedis使用示例

---
+++ spring-boot-redis/src/main/java/com/neo/job/NewJob.java
@@ -0,0 +1,43 @@
+package com.neo.job;
+
+import com.neo.utils.RedisUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Auther: bcl
+ * @Description:
+ * @Date: Create in 8:52 下午 2021/1/19
+ */
+@Component
+public class NewJob {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NewJob.class);
+
+    @PostConstruct
+    public void init() throws ExecutionException, InterruptedException {
+
+        CompletableFuture<Boolean> feature = null;
+        for (int i = 0; i < 3; i++) {
+            if (feature==null || !feature.get()){
+                LOGGER.info("-------{}----",i);
+                feature = CompletableFuture.supplyAsync(() -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(30);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return false;
+                });
+            }
+        }
+    }
+}
